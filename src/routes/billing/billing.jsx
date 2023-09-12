5c023afea090942c03f5b18b9f95a24089368c17
= <johnnyopeyemi@gmail.com>
2023-09-12 12:30:28 +0100

    finished latest design

--- src/routes/billing/billing.jsx
+++ src/routes/billing/billing.jsx
@@ -6,7 +6,7 @@ const Billing = () => {
     <>
       <div className="relative bg-white max-w-screen-2xl mx-auto h-auto">
         <Mainnav />
-        <div className="w-[90%] mx-auto sm:p-6 grid grid-cols-1 md:grid-cols-2 gap-28">
+        <div className="w-[90%] mx-auto sm:p-6 grid grid-cols-1 md:grid-cols-2 gap-28 mt-5">
           {/* checkout */}
           <div className="w-full">
             <h3 className="opensans text-[#4F46BA] font-bold">Checkout</h3>
@@ -146,17 +146,57 @@ const Billing = () => {
                 </div>
               </div>
               <div className="w-full">
-                <label htmlFor="companyName" className="opensans flex gap-3 text-[#333333] font-bold">
-                  <input type="checkbox" id="companyName" name="companyName" />
+                <label
+                  htmlFor="companyName"
+                  className="opensans flex gap-3 text-[#333333] font-bold"
+                >
+                  <input type="checkbox" id="companyName" name="companyName" className="w-5 h-5 cursor-pointer"/>
                   Ship to a different address
                 </label>
               </div>
             </div>
           </div>
-            {/* order summary */}
-            <div className="w-full">
-                <h3 className="opensans text-3xl font-bold mt-3 mb-5">Order Review</h3>
-            </div>    
+          {/* order summary */}
+          <div className="w-full mb-36">
+            <h3 className="opensans text-3xl font-bold sm:mt-7 mb-5">
+              Order Details
+            </h3>
+            <div className="w-full flex flex-col gap-5">
+              <div className="w-full flex justify-between border-b-2 border-b-[#E0E0E0] pb-3">
+                <p className="monteserrat text-[#1C1C1C] font-semibold">
+                  Product
+                </p>
+                <p className="monteserrat text-[#1C1C1C] font-semibold">
+                  Price
+                </p>
+              </div>
+              <div className="w-full flex justify-between border-b-2 border-b-[#E0E0E0] pb-3">
+                <p className="monteserrat text-[#AEACAC] font-semibold">Card</p>
+                <p className="monteserrat text-[#AEACAC] font-semibold">
+                  30 AED
+                </p>
+              </div>
+              <div className="w-full flex justify-between border-b-2 border-b-[#E0E0E0] pb-3">
+                <p className="monteserrat text-[#AEACAC] font-semibold">Tax</p>
+                <p className="monteserrat text-[#AEACAC] font-semibold">
+                  5 AED
+                </p>
+              </div>
+              <div className="w-full flex justify-between pb-3">
+                <p className="monteserrat text-[#1C1C1C] font-semibold">
+                  Subtotal
+                </p>
+                <p className="monteserrat text-[#1C1C1C] font-semibold">
+                  35 AED
+                </p>
+              </div>
+              <div className="flex w-full justify-end">
+                <button className=" bg-[#4F46BA] text-white py-3 rounded-[9px] font-bold text-md manrope px-5">
+                  Place Order
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
